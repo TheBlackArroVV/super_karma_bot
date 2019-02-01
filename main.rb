4e5cbd3710dbd15958f1b19d8005d8de70487e4a
TheBlackArroVV <theblackarrovv@gmail.com>
2019-02-01 15:35:38 +0200

    delete reply to message

--- main.rb
+++ main.rb
@@ -7,14 +7,12 @@ token = '649271734:AAGeC6IwUFARpqwy41Mv-QgR7WYyqdHH22M'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    pp message.text
     case message.text
     when '++'
       if message.reply_to_message
         pp 'here'
         bot.api.sendMessage(
           chat_id: message.chat.id,
-          reply_to_message_id: message.message_id,
           text: "@#{message.reply_to_message.from.username} karma growth"
         )
         db.save_to_db_result(message.reply_to_message.from.username)
