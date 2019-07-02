258d0654c1fe1ef11325dd9d1d26945ab8791d90
Yaroslav Liakh <liakhyaroslav@gmail.com>
2019-07-02 15:56:35 +0300

    Fix bug when counter stop count.

--- db/database.rb
+++ db/database.rb
@@ -6,22 +6,22 @@ class Database
   end
 
   def increase_karma(user_name)
-    user = @users.where(user_name: user_name).first
+    user = users.where(user_name: user_name)
 
-    if user
-      user.update(count: user[:count] + 1)
+    if user.first
+      user.update(count: user.first[:count] + 1)
     else
-      @users.insert(user_name: user_name, count: 1)
+      users.insert(user_name: user_name, count: 1)
     end
   end
 
   def decrease_karma(user_name)
-    user = @users.where(user_name: user_name).first
+    user = users.where(user_name: user_name)
 
-    if user
-      user.update(count: user[:count] - 1)
+    if user.first
+      user.update(count: user.first[:count] - 1)
     else
-      @users.insert(user_name: user_name, count: 0)
+      users.insert(user_name: user_name, count: 0)
     end
   end
 
@@ -43,8 +43,6 @@ class Database
     @db.create_table? :users do
       primary_key :id
       String :user_name
-      String :first_name
-      String :last_name
       Integer :count
     end
   end
